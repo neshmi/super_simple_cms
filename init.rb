dc4113ea9fda827c4f845a69fa0def7739cfd622
Matt Vincent <matt@averails.com>
2008-05-30 17:49:21 -0500

    modifying init.rb

--- init.rb
+++ init.rb
@@ -11,12 +11,11 @@ ActiveRecord::Base.send :include, ActiveRecord::Acts::Tree
 ActiveRecord::Base.class_eval { include ActiveRecord::Acts::List }
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:setting] = '%b %d'
-# ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:arrow_billing]="%Y%m%d"
 
 ActionController::Base.send(:include, SuperSimpleCms::SuperSimple) 
 ActionController::Base.send(:include, SuperSimpleCMS::Controllers)
 ActionController::Base.send(:include, SuperSimpleCMS::Models)
-ActionController::Base.view_paths.unshift(File.join(File.dirname(__FILE__), 'lib','super_simple_cms','views'))
+ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'lib','super_simple_cms','views'))
 
 ActionView::Base.send(:include, SuperSimpleCMS::Helpers::NavigationHelper)
 
