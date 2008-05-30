5b5a6ba459565592954e1d0ad14c96fc74e66488
Matt Vincent <matt@averails.com>
2008-05-29 21:16:29 -0500

    updating readme, assets and giving the rake task to copy assets over

--- lib/super_simple_cms/group.rb
+++ lib/super_simple_cms/group.rb
@@ -4,4 +4,9 @@ class SuperSimpleCms::Group < ActiveRecord::Base
   validates_presence_of :group_name
   has_many :pages, :order=>:position
   has_many :links, :class_name => "Page", :foreign_key => "group_id", :conditions=>['linkable = ?', true], :order=>:position
+  
+  def validate
+    
+  end
+  
 end
--- lib/super_simple_cms/routing.rb
+++ lib/super_simple_cms/routing.rb
@@ -1,11 +1,11 @@
 module SuperSimpleCMS
   module Routing
-    def from_plugin(name) 
+    def super_simple_cms 
       plugin_root = File.join(RAILS_ROOT, 
                               'vendor', 
                               'plugins') 
       routes_path = File.join(plugin_root, 
-                              name.to_s, 
+                              'super_simple_cms', 
                               'routes.rb') 
       if File.file?(routes_path) 
         eval(IO.read(routes_path), binding, routes_path) 
