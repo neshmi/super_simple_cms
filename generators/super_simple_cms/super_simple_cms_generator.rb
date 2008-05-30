1e7785e9f40d779d92fcf746faf9a4664cdbf53b
Matt Vincent <matt@averails.com>
2008-05-29 21:37:36 -0500

    fixing generator

--- generators/super_simple_cms/super_simple_cms_generator.rb
+++ generators/super_simple_cms/super_simple_cms_generator.rb
@@ -2,7 +2,6 @@ class SuperSimpleCmsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name=>'create_super_simple_cms'
-      m.from_plugin 'super_simple_cms'
     end
   end
 end
