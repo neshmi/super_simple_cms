af2fc182dcfa1625bee88157e8efa6679d856f07
Matt Vincent <matt@averails.com>
2008-09-16 18:09:27 -0500

    updating css in nav helper

--- lib/super_simple_cms/helpers/navigation_helper.rb
+++ lib/super_simple_cms/helpers/navigation_helper.rb
@@ -6,18 +6,18 @@ module SuperSimpleCMS::Helpers::NavigationHelper
     if @links && @links.length > 0
       if @links.first.group != SuperSimpleCms::Group.find(:first, :order=>'position')
         @links.each do |link|
-          html << "<li #{css_class}>#{link_to link.title, view_group_path(:page_group=>link.group.group_name, :perma_link=>link.perma_link)}</li>"
+          html << "<li class='#{css_class}'>#{link_to link.title, view_group_path(:page_group=>link.group.group_name, :perma_link=>link.perma_link)}</li>"
         end
       else
         @links.each do |link|
-          html << "<li #{css_class}>#{link_to link.title, view_page_path(link.perma_link)}</li>"
+          html << "<li class='#{css_class}'>#{link_to link.title, view_page_path(link.perma_link)}</li>"
         end
       end
     end
     
     if @group_links && !@group_links.empty?
       @group_links.each do |group_link|
-        html << "<li>#{link_to group_link.group.group_name, view_group_path(:page_group=>group_link.group.group_name, :perma_link=>group_link.perma_link) if @links.first.group != group_link.group}</li>"
+        html << "<li class='#{css_class}'>#{link_to group_link.group.group_name, view_group_path(:page_group=>group_link.group.group_name, :perma_link=>group_link.perma_link) if @links.first.group != group_link.group}</li>"
       end
     end
 
